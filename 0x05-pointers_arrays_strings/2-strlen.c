397d3e6b0ce07011753cd01b0422144081cac187
Ruthnnamani <nnamanipruth@gmail.com>
2023-07-24 16:38:30 -0700

    2-strlen.c

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _strlen - length of a string
+ * @s: returns the length
+ * Return: length of the string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (*s != '\0')
+	{
+		i++;
+		s++;
+	}
+	return (i);
+}
