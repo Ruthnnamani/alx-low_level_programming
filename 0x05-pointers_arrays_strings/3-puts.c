18bab42a21a6a4a65cbde4c926d6443f99dd56f6
Ruthnnamani <nnamanipruth@gmail.com>
2023-07-24 16:56:23 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * _puts - prints a string
+ * @str: string
+ * Return: void
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		putchar(*str++);
+	}
+	putchar('\n');
+}
