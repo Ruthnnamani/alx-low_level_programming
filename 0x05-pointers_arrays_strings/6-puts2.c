7199a6e6dc6647c2a9519aeed0718dc0f816c408
Ruthnnamani <nnamanipruth@gmail.com>
2023-07-24 17:57:30 -0700

    6-puts2.c

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -1,11 +1,9 @@
 #include "main.h"
-
 /**
  * puts2 - print other character of a string
  * @str: starting with the first character
  * Return: Always 0
  */
-
 void puts2(char *str)
 {
 	int i = 0;
