527badb9b954f59edbf06e9a0244f8625cce72c4
Ruthnnamani <nnamanipruth@gmail.com>
2023-07-31 16:37:24 -0700

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -7,13 +7,13 @@
  */
 char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
-	{
-		if (*s == c)
-		{
-			return (s);
-		}
-		s++;
-	}
+	char *s1 = s - 1;
+
+	do {
+		s1++;
+		if (*s1 == c)
+			return (s1);
+	} while (*s1 != '\0');
+
 	return (0);
 }
